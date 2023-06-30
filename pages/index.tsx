cef254e7021c61c2c22a1668b389a86876b6b7a3
Lisa Gee <zhusufeng@gmail.com>
2023-06-30 12:38:16 -0700

    Create Breakfast form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Form, Button, Select } from "antd";
-import { BREAKFAST_ENTREES } from "./data/food";
+import { BREAKFAST_ENTREES, BREAKFAST_SIDES } from "./data/food";
 
 export default function Home() {
   const onFinish = (values: any) => {
@@ -14,9 +14,35 @@ export default function Home() {
         onFinish={onFinish}
         style={{ maxWidth: 600 }}
       >
-        <Form.Item name="breakfastEntree" label="Breakfast Entree">
+        <Form.Item
+          name="breakfastEntree"
+          label="Breakfast Entree"
+          rules={[{ required: true }]}
+        >
           <Select>
             {BREAKFAST_ENTREES.map(el => {
+              return (
+                <Select.Option key={el} value={el}>
+                  {el}
+                </Select.Option>
+              );
+            })}
+          </Select>
+        </Form.Item>
+        <Form.Item name="breakfastSide1" label="Breakfast Side #1">
+          <Select>
+            {BREAKFAST_SIDES.map(el => {
+              return (
+                <Select.Option key={el} value={el}>
+                  {el}
+                </Select.Option>
+              );
+            })}
+          </Select>
+        </Form.Item>
+        <Form.Item name="breakfastSide2" label="Breakfast Side #2">
+          <Select>
+            {BREAKFAST_SIDES.map(el => {
               return (
                 <Select.Option key={el} value={el}>
                   {el}
