021b166d0215a6504496f1dccc73f73ccf830875
Lisa Gee <zhusufeng@gmail.com>
2023-07-28 22:08:53 -0700

    Update Layout to use Navbar

--- pages/components/Layout.tsx
+++ pages/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Layout } from "antd";
-const { Header, Content, Footer } = Layout;
+import Navbar from "./Navbar";
 
 type Props = {
   children: React.ReactNode;
@@ -7,14 +7,14 @@ type Props = {
 
 const MainLayout = ({ children }: Props) => {
   return (
-    <Layout className="layout" style={{ height: "100vh", width: "100vw" }}>
-      <Header style={{ display: "flex", alignItems: "center" }}>
-        <div>Breakfast</div>
-      </Header>
-      <Content style={{ padding: "50px" }}>
+    <Layout className="layout" style={{ minHeight: "100vh" }}>
+      <Navbar />
+      <Layout.Content style={{ padding: "50px" }}>
         <div className="site-layout-content">{children}</div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>(C) 2023 Lisa</Footer>
+      </Layout.Content>
+      <Layout.Footer style={{ textAlign: "center" }}>
+        (C) 2023 Lisa
+      </Layout.Footer>
     </Layout>
   );
 };
