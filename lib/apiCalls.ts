6e7791f76bd00d5145eb7867669d19375073735a
Lisa Gee <zhusufeng@gmail.com>
2023-08-13 08:25:35 -0700

    Remove catch in apiCalls so they can be surfaced in components

--- lib/apiCalls.ts
+++ lib/apiCalls.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 export async function addDish(payload: DishPayload, dishId: null) {
-  await axios.post("/api/dish", payload).catch(error => console.log(error));
+  await axios.post("/api/dish", payload);
 }
 
 export async function editDish(payload: DishPayload, dishId: string) {
-  await axios
-    .put(`/api/dish/${dishId}`, payload)
-    .catch(error => console.log(error));
+  await axios.put(`/api/dish/${dishId}`, payload);
 }
