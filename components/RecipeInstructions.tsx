c945afa9c98f7e2df20d787ab8e44c20735af733
Lisa Gee <zhusufeng@gmail.com>
2023-08-17 09:26:40 -0700

    Remove bullets in RecipeInstructions

--- components/RecipeInstructions.tsx
+++ components/RecipeInstructions.tsx
@@ -6,7 +6,13 @@ const RecipeInstructions = (props: Props) => {
 
   if (!instructions || !instructions.length) return "No instructions included";
   return (
-    <ul>
+    <ul
+      style={{
+        listStyleType: "none",
+        padding: 0,
+        margin: 0,
+      }}
+    >
       {instructions.map((step, idx) => {
         return <li key={`${idx}${step}`}>{step}</li>;
       })}
