c9561af8fcc0ef52a27a58320d804f29616955a4
Lisa Gee <zhusufeng@gmail.com>
2023-09-04 17:04:23 -0700

    Add try/catch for signIn and jwt callbacks as they use async/await

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,33 +10,46 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  // https://next-auth.js.org/configuration/callbacks
   callbacks: {
     async signIn(data) {
-      // TRY CATCH
-      const { profile } = data;
-      const { given_name, family_name, email } = profile;
-      await dbConnect();
-      const result = await User.find({ email }, ["_id"]);
-      // If no user is found, add them to the database
-      if (!result.length) {
-        await User.create({
-          firstName: given_name,
-          lastName: family_name,
-          email: email,
-        });
+      try {
+        const { profile } = data;
+        const { given_name, family_name, email } = profile;
+        await dbConnect();
+        const result = await User.find({ email }, ["_id"]);
+        // If no user is found, add them to the database
+        if (!result.length) {
+          await User.create({
+            firstName: given_name,
+            lastName: family_name,
+            email: email,
+          });
+        }
+        return true;
+      } catch (error) {
+        // TODO add to logs
+        console.log(error);
+        return false;
       }
-      return true;
     },
     async jwt({ token }) {
-      // TRY CATCH
-      const result = await User.find({ email: token.email }, ["_id"]);
-      if (result.length) {
-        const [user] = result;
-        token.userId = user._id.toString();
+      try {
+        const result = await User.find({ email: token.email }, ["_id"]);
+        if (result.length) {
+          const [user] = result;
+          token.userId = user._id.toString();
+        }
+        return token;
+      } catch (error) {
+        // TODO add to logs
+        console.log(error);
+        token.userId = null;
+        return token;
       }
-      return token;
     },
     async session({ session, token }) {
+      // Expose user id to client
       session.user.id = token.userId;
       return session;
     },
