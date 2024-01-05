0df8be5f4c83e78eed4e47106a7814dda92bbcf9
Lisa Gee <zhusufeng@gmail.com>
2024-01-04 22:14:25 -0800

    Add columns to plan page table

--- pages/plan.tsx
+++ pages/plan.tsx
@@ -1,5 +1,35 @@
+import { Table } from "antd";
+import type { ColumnsType } from "antd/es/table";
 import { useSession } from "next-auth/react";
 import Layout from "../components/Layout";
+
+const columns: ColumnsType = [
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+  },
+  {
+    title: "Breakfast",
+    dataIndex: "breakfast",
+    key: "breakfast",
+  },
+  {
+    title: "Lunch",
+    dataIndex: "lunch",
+    key: "lunch",
+  },
+  {
+    title: "Dinner",
+    dataIndex: "dinner",
+    key: "dinner",
+  },
+  {
+    title: "Snack",
+    dataIndex: "snack",
+    key: "snack",
+  },
+];
 
 // TODO Keep view logic in a component!
 const Plan: React.FC = () => {
@@ -11,7 +41,11 @@ const Plan: React.FC = () => {
       </Layout>
     );
   }
-  return <Layout>Logged In. Plan Page.</Layout>;
+  return (
+    <Layout>
+      <Table columns={columns} />
+    </Layout>
+  );
 };
 
 export default Plan;
