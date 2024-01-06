e8d5501826be91650fd325b772116fa32578f1d6
Lisa Gee <zhusufeng@gmail.com>
2024-01-06 00:29:02 -0800

    Populate data in GET /meals and practice transforming data

--- pages/api/meals/index.ts
+++ pages/api/meals/index.ts
@@ -1,7 +1,9 @@
 import dayjs from "dayjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "../../../lib/dbConnect";
+import { transformMeals } from "../../../lib/mealHelpers";
 import Dish from "../../../models/dish.model";
+import User from "../../../models/user.model";
 import UserMeal from "../../../models/userMeal.model";
 
 export default async function handler(
@@ -28,7 +30,11 @@ export default async function handler(
             $lt: endDate.toISOString(),
           },
         })
-          .populate("userId")
+          .populate({
+            path: "userId",
+            model: User.modelName,
+            select: "firstName",
+          })
           .populate("mealType")
           .populate({
             path: "entrees",
@@ -36,7 +42,33 @@ export default async function handler(
             select: "name",
           });
         // TODO Transform data into what frontend needs
-        console.log("result", result);
+        const pracData = [
+          {
+            _id: "6598fb690c3b629da31a32fe",
+            userId: {
+              _id: "64d8535357bde9dc7ae69a18",
+              firstName: "Lisa",
+            },
+            mealDate: "2024-01-06T06:54:25.895Z",
+            mealType: {
+              _id: "659793e54faff8b5b2de226a",
+              type: "breakfast",
+            },
+            entrees: [
+              {
+                _id: "64d7ae7e611c0082d1339386",
+                name: "Eggs, scrambled",
+              },
+            ],
+            sides: [],
+            createdAt: "2024-01-06T07:04:09.880Z",
+            updatedAt: "2024-01-06T07:04:09.880Z",
+            __v: 0,
+          },
+        ];
+        transformMeals(pracData);
+        // console.log("result", result);
+        // const newResult = transformMeals(result);
         res.status(200).json({ success: true, data: result });
       } catch (error) {
         res.status(400).json({
@@ -57,8 +89,6 @@ export default async function handler(
           mealType,
           entrees,
           sides,
-          createdBy: userId,
-          updatedBy: userId,
         });
         res.status(201).json({ success: true, data: userMeal });
       } catch (error) {
